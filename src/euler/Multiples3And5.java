e827a3118d1379e4c34bb33a02a996f9be23fd6d
seafea <seafea@vt.edu>
2020-01-07 11:19:26 -0500

    Started Project Euler

---
+++ src/euler/Multiples3And5.java
@@ -0,0 +1,17 @@
+package euler;
+
+public class Multiples3And5 {
+
+	/**
+	 * 
+	 * If we list all the natural numbers below 10 that 
+	 * are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of 
+	 * these multiples is 23.
+	 * Find the sum of all the multiples of 3 or 5 below 1000.
+	 */
+	public static int Multiples3And5Simple(int maxNum)
+	{
+		for (int i = 0;)
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ src/test/TestMultiples3And5.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import euler.Multiples3And5;
+
+import org.junit.jupiter.api.Test;
+
+class TestMultiples3And5 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
