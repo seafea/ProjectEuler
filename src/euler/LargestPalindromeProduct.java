bd16b7367a7c69c4c44078bd6186a5d8ea1912d8
seafea <seafea@vt.edu>
2020-01-07 16:41:45 -0500

    created a simple solution

--- src/euler/LargestPalindromeProduct.java
+++ src/euler/LargestPalindromeProduct.java
@@ -1,9 +1,23 @@
 package euler;
+import sharedFunctions.SharedFunctions;
 
 public class LargestPalindromeProduct {
 
 	public static int findLargestPalindromeSimple()
 	{
-		throw new UnsupportedOperationException();
+		int largestPalindrome = 1;
+		for (int i = 100; i < 1000; i++)
+		{
+			for (int j = i; j < 1000; j++)
+			{
+				int possibleNum = i * j;
+				if (SharedFunctions.isaPalindrome(possibleNum))
+				{
+					largestPalindrome = possibleNum > 
+						largestPalindrome ? possibleNum : largestPalindrome;
+				}
+			}
+		}
+		return largestPalindrome;
 	}
 }
