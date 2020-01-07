6767bf731aec7787431d7e53839808111af3ac0c
seafea <seafea@vt.edu>
2020-01-07 16:08:54 -0500

    Added another way to solve it

--- src/euler/LargestPrimeFactor.java
+++ src/euler/LargestPrimeFactor.java
@@ -3,6 +3,48 @@ import sharedFunctions.SharedFunctions;
 
 public class LargestPrimeFactor {
 
+	
+	public static long findLargestPrimeFactorDivision(long num)
+	{
+		int factor = 2;
+		int lastFactor = 1;
+		if (num % 2 == 0)
+		{
+			num /= 2;
+			lastFactor = 2;
+			while (num % 2 == 0)
+			{
+				num /= 2;
+			}
+		}
+		while (num > 1)
+		{
+			if (num % factor == 0)
+			{
+				lastFactor = factor;
+				num /= factor;
+				while (num % factor == 0)
+				{
+					num /= factor;
+				}
+			}
+			factor++;
+		}
+		return lastFactor;
+	}
+	
+	public static long findLargestPrimeFactorWorst(long num)
+	{
+		for (long i = num; i > 1; i--)
+		{
+			if (num % i == 0 && isItPrime(i))
+			{
+				return i;
+			}
+		}
+		return num;
+	}
+	
 	public static long findLargestPrimeFactorSimple(long num)
 	{
 		int startingNum = (int)Math.ceil(Math.sqrt(num));
