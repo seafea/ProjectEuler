8f73d75e45f2255c2cae861eb3a8e8c8d0f4a309
seafea <seafea@vt.edu>
2020-01-17 14:56:15 -0500

    Added Unit Tests for Special Pythagorean Triplets

---
+++ src/test/SpecialPythagoreanTripletTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import euler.SpecialPythagoreanTriplet;
+
+import org.junit.jupiter.api.Test;
+
+class SpecialPythagoreanTripletTest {
+
+	@Test
+	void findSpecialPythagoreanTripletBasicTest() {
+		assertEquals(60, SpecialPythagoreanTriplet.findSpecialPythagoreanTriplet(12));
+	}
+	
+	@Test
+	void findSpecialPythagoreanTripletBetterBasicTest() {
+		assertEquals(60, SpecialPythagoreanTriplet.findSpecialPythagoreanTripletBetter(12));
+	}
+	
+	@Test
+	void findSpecialPythagoreanTripletProblemTest() {
+		assertEquals(31875000, SpecialPythagoreanTriplet.findSpecialPythagoreanTriplet(1000));
+	}
+	
+	@Test
+	void findSpecialPythagoreanTripletBetterProblemTest() {
+		assertEquals(31875000, SpecialPythagoreanTriplet.findSpecialPythagoreanTripletBetter(1000));
+	}
+
+}
