49b5804fbd12d7c13ca72841956e18127aa65b11
seafea <seafea@vt.edu>
2020-02-11 15:44:31 -0500

    Finished Number Letter Counts

--- src/euler/NumberLetterCounts.java
+++ src/euler/NumberLetterCounts.java
@@ -2,10 +2,10 @@ package euler;
 
 public class NumberLetterCounts {
 
-	public static int[] firstNineteenValues = new int[] {
+	private static int[] firstNineteenValues = new int[] {
 			3, 3, 5, 4, 4, 3, 5, 5, 4, 3, 6, 6, 8, 8, 7, 7, 9, 8, 8
 	};
-	public static int[] tens = new int[] {
+	private static int[] tens = new int[] {
 			6, 6, 5, 5, 5, 7, 6, 6
 	};
 	public static int getNumberLetterCounts(int ceiling)
@@ -20,29 +20,80 @@ public class NumberLetterCounts {
 		return sumOfLetters;
 	}
 	
-	public static int getNumberOfLetters(int number)
+	private static int getNumFrom1stDigitOrInitial(int number)
 	{
+		int returnValue = -1;
 		if (number < firstNineteenValues.length + 1)
 		{
-			return firstNineteenValues[number - 1];
+			returnValue = firstNineteenValues[number - 1];
 		}
-		else if (number >= firstNineteenValues.length + 1 && 
-				number <= 99 && number % 10 == 0)
+		else
 		{
-			return tens[(number / 10) - 2];
+			returnValue = firstNineteenValues[number - 1];
 		}
-		else if (number >= firstNineteenValues.length + 1 && number <= 99)
+		return returnValue;
+	}
+	
+	private static int getNumFrom2ndDigit(int number)
+	{
+		int returnValue = -1;
+		returnValue = tens[(number / 10) - 2];
+		if (number % 10 != 0)
 		{
-			int numLetters = 0;
-			int tensDigit = number / 10;
-			numLetters += tens[tensDigit - 2];
-			numLetters += firstNineteenValues[(number % 10) - 1];
-			return numLetters;
+			returnValue += getNumFrom1stDigitOrInitial(number % 10);
 		}
-		else if (number >= 100 && number <= 999 && number % 100 == 0)
+		return returnValue;
+	}
+	
+	private static int getNumFrom3rdDigit(int number)
+	{
+		int returnValue = firstNineteenValues[(number / 100) - 1];
+		returnValue += 7;
+		if (number % 100 != 0)
 		{
-			
+			returnValue += 3;
+			if (number % 100 > 19)
+			{
+				returnValue += getNumFrom2ndDigit(number % 100);
+			}
+			else
+			{
+				returnValue += getNumFrom1stDigitOrInitial(number % 100);
+			}
 		}
-		return 0;
+		return returnValue;
+	}
+	
+	private static int getNumFrom4thDigit(int number)
+	{
+		int returnValue = firstNineteenValues[(number / 1000) - 1];
+		returnValue += 8;
+		if (number % 1000 != 0)
+		{
+			returnValue += getNumFrom3rdDigit(number % 1000);
+		}
+		return returnValue;
+	}
+	
+	private static int getNumberOfLetters(int number)
+	{
+		int returnValue = -1;
+		if (number < firstNineteenValues.length + 1)
+		{
+			returnValue = getNumFrom1stDigitOrInitial(number); 
+		}
+		else if (number > 19 && number < 100)
+		{
+			returnValue = getNumFrom2ndDigit(number);
+		}
+		else if (number > 99 && number < 1000)
+		{
+			returnValue = getNumFrom3rdDigit(number);
+		}
+		else if (number > 999 && number < 10000)
+		{
+			returnValue = getNumFrom4thDigit(number);
+		}
+		return returnValue;
 	}
 }
--- src/test/NumberLetterCountsTest.java
+++ src/test/NumberLetterCountsTest.java
@@ -8,15 +8,25 @@ import org.junit.jupiter.api.Test;
 class NumberLetterCountsTest {
 
 	@Test
-	void test() {
+	void getNumberLetterCountsTest() {
 		assertEquals(19, NumberLetterCounts.getNumberLetterCounts(5));
 	}
-
+	
 	@Test
-	void getNumberOfLettersTest() {
-		assertEquals(9, NumberLetterCounts.getNumberOfLetters(17));
-		assertEquals(8, NumberLetterCounts.getNumberOfLetters(19));
-		assertEquals(6, NumberLetterCounts.getNumberOfLetters(20));
-		assertEquals(9, NumberLetterCounts.getNumberOfLetters(21));
+	void getNumberLetterCounts() {
+		assertEquals(21124, NumberLetterCounts.getNumberLetterCounts(1000));
 	}
+
+	//@Test
+	//void getNumberOfLettersTest() {
+		//assertEquals(9, NumberLetterCounts.getNumberOfLetters(17));
+		//assertEquals(8, NumberLetterCounts.getNumberOfLetters(19));
+		//assertEquals(6, NumberLetterCounts.getNumberOfLetters(20));
+		//assertEquals(9, NumberLetterCounts.getNumberOfLetters(21));
+		//assertEquals(10, NumberLetterCounts.getNumberOfLetters(57));
+		//assertEquals(10, NumberLetterCounts.getNumberOfLetters(100));
+		//assertEquals(22, NumberLetterCounts.getNumberOfLetters(117));
+		//assertEquals(23, NumberLetterCounts.getNumberOfLetters(555));
+		//assertEquals(11, NumberLetterCounts.getNumberOfLetters(1000));
+	//}
 }
