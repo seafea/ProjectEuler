6c250ca93ad9b64128561bd485ebe0f34465598b
seafea <seafea@vt.edu>
2020-02-13 13:30:47 -0500

    Amicable Numbers solved

--- src/euler/AmicableNumbers.java
+++ src/euler/AmicableNumbers.java
@@ -1,5 +1,44 @@
 package euler;
 
+import java.util.ArrayList;
+
 public class AmicableNumbers {
 
+	public static int findSumOfAmicableNumbersUnderN(int n)
+	{
+		int sumOfAmicableNumbers = 0;
+		for (int i = 1; i < n; i++)
+		{
+			if (isNumberAmicable(i))
+			{
+				System.out.println(i + " is an amicable number.");
+				sumOfAmicableNumbers += i;
+			}
+		}
+		return sumOfAmicableNumbers;
+	}
+	
+	private static boolean isNumberAmicable(int number)
+	{
+		int sumOfProperDivisors = findSumOfProperDivisors(number);
+		return (findSumOfProperDivisors(sumOfProperDivisors) == number && sumOfProperDivisors != number);
+	}
+	
+	private static int findSumOfProperDivisors(int number)
+	{
+		ArrayList<Integer> listOfDivisors = new ArrayList<Integer>();
+		int sumOfProperDivisors = 0; 
+		for (int i = 1; i <= number / 2; i++)
+		{
+			if (number % i == 0)
+			{
+				listOfDivisors.add(i);
+			}
+		}
+		for (int i : listOfDivisors)
+		{
+			sumOfProperDivisors += i;
+		}
+		return sumOfProperDivisors;
+	}
 }
--- src/test/AmicableNumbersTest.java
+++ src/test/AmicableNumbersTest.java
@@ -1,14 +1,31 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import euler.AmicableNumbers;
 
 import org.junit.jupiter.api.Test;
 
 class AmicableNumbersTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void findSumOfAmicableNumbersUnderNTest() {
+		assertEquals(8442, AmicableNumbers.findSumOfAmicableNumbersUnderN(2925));
+	}
+	
+	@Test
+	void findSumOfAmicableNumbersUnderNProblemTest() {
+		assertEquals(10, AmicableNumbers.findSumOfAmicableNumbersUnderN(10000));
+	}
+	
+	@Test
+	void findSumOfProperDivisorsTest() {
+		//assertEquals(284, AmicableNumbers.findSumOfProperDivisors(220));
+	}
+	
+	@Test
+	void isNumberAmicableTest() {
+		//assertTrue(AmicableNumbers.isNumberAmicable(220));
+		//assertTrue(AmicableNumbers.isNumberAmicable(284));
 	}
 
 }
