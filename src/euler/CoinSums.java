3a80fddbbc3b3793959db15d26869d2755c73409
seafea <seafea@vt.edu>
2020-03-19 16:54:09 -0400

    end of the day

---
+++ src/euler/CoinSums.java
@@ -0,0 +1,15 @@
+package euler;
+
+public class CoinSums {
+
+	public static int numDifferentWaysToGet(int value)
+	{
+		int[] coinValues = new int[] {
+				1, 2, 5, 10, 20, 50, 100, 200
+		};
+		int[] myCoins = new int[] {
+			200, 0, 0, 0, 0, 0, 0, 0	
+		};
+		throw new UnsupportedOperationException();
+	}
+}
